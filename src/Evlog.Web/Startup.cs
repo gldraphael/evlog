4d92a249f521ea81d9624ce439312528b7315a86
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-04-30 21:17:18 +0530

    Use lowercase page urls

--- src/Evlog.Web/Startup.cs
+++ src/Evlog.Web/Startup.cs
@@ -22,7 +22,8 @@ namespace Evlog.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddRouting(options => options.LowercaseUrls = true)
+                    .AddMvc();
 
             services.AddTransient<IEventsService, EventsService>();
         }
