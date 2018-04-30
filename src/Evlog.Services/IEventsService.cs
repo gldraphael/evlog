ff5d67a2fabad12d3838812fef0aab6c2b2f1dc6
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-04-30 20:34:37 +0530

    Add page for past events

--- src/Evlog.Services/IEventsService.cs
+++ src/Evlog.Services/IEventsService.cs
@@ -6,5 +6,7 @@ namespace Evlog.Services
     public interface IEventsService
     {
          List<Event> Get();
+        List<Event> GetUpcomingEvents();
+        List<Event> GetPastEvents();
     }
 }
