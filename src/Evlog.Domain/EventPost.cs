b175664148d1b58a7c5112d48acd835734c5ff66
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-05-14 00:10:49 +0530

    Don't show the full body text

--- src/Evlog.Domain/EventPost.cs
+++ src/Evlog.Domain/EventPost.cs
@@ -5,9 +5,10 @@ namespace Evlog.Domain
     public class EventPost
     {
         public int Id { get; set; }
-        public DateTime TimeStamp { get; set; } = DateTime.UtcNow;
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
         public string Title { get; set; }
         public string Body { get; set; }
+        public string Excerpt { get; set; }
         public DateTime StartDateTime { get; set; }
         public DateTime? EndDate { get; set; }
         public DateTime? EndTime { get; set; }
