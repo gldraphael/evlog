8bba09e4e26bda44c1ec78cbb4cab1a6a14e70b2
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-07-06 12:25:01 +0530

    Add a UseAuthentication option

--- src/Evlog.Infrastructure/MongoConfig.cs
+++ src/Evlog.Infrastructure/MongoConfig.cs
@@ -4,10 +4,19 @@ namespace Evlog.Infrastructure
     {
         public string Host { get; set; }
         public string Port { get; set; }
+        public bool UseAuthentication { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public int ConnectTimeout { get; set; } = 1000;
-        public string ConnectionString => $"mongodb://{Username}:{Password}@{Host}:{Port}?connectTimeoutMS={ConnectTimeout}";
+        public string ConnectionString
+        {
+            get
+            {
+                if(UseAuthentication)
+                    return $"mongodb://{Username}:{Password}@{Host}:{Port}?connectTimeoutMS={ConnectTimeout}";
+                return $"mongodb://{Host}:{Port}?connectTimeoutMS={ConnectTimeout}";
+            }
+        }
 
         public string Database { get; set; }
     }
