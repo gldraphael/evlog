3becba0fbe5af3001db2b4951e211eec5fcc5b0f
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-07-09 11:24:11 +0530

    Cleanup the Startup class
    Move logic from the startup class to separate extension methods.

--- src/Evlog.Web/Startup.cs
+++ src/Evlog.Web/Startup.cs
@@ -1,56 +1,25 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Evlog.Domain;
-using Evlog.Domain.Queries;
-using Evlog.Infrastructure;
-using Evlog.Infrastructure.Queries;
+using Evlog.Web.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using MongoDB.Driver;
 
 namespace Evlog.Web
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRouting(options => options.LowercaseUrls = true)
-                    .AddMvc()
-                    .AddRazorPagesOptions(options => {
-                        options.Conventions.AddPageRoute("/Events/View", "{slug}");
-                    });
-
-            var appsettings = Configuration.GetSection("AppSettings").Get<AppSettings>();
-            if(appsettings.UseMongo)
-            {
-                var configSection = Configuration.GetSection("Mongo");
-                var config = configSection.Get<MongoConfig>();
-                var mongoClient = new MongoClient(connectionString: config.ConnectionString) as IMongoClient;
-                var database = mongoClient.GetDatabase(config.Database);
-                var eventsCollection = database.GetCollection<EventPost>("Events");
-
-                services.Configure<MongoConfig>(configSection);
-                services.AddSingleton(mongoClient);
-                services.AddSingleton(database);
-                services.AddSingleton(eventsCollection);
-            }
-
-            services.AddTransient<IAllEventsQuery, AllEventsQuery>();
-            services.AddTransient<IPastEventsQuery, PastEventsQuery>();
-            services.AddTransient<IUpcomingEventsQuery, UpcomingEventsQuery>();
-            services.AddTransient<IEventQuery, EventQuery>();
+            services.AddEvlogMvc();
+            services.AddMongo(Configuration);
+            services.AddEvlogQueries();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
