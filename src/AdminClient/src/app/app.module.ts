dc5a0e847492615f66fd6d04b77deb433f74763d
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-07-23 20:31:38 +0530

    Create header and footer components

--- src/AdminClient/src/app/app.module.ts
+++ src/AdminClient/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavComponent } from './shared/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
