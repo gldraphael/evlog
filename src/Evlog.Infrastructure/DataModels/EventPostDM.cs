a61b18ef0abf1484bbbed0040427e924dc0538d0
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-08-08 20:45:16 +0530

    Remove getter only props from EventPostDM

--- src/Evlog.Infrastructure/DataModels/EventPostDM.cs
+++ src/Evlog.Infrastructure/DataModels/EventPostDM.cs
@@ -15,11 +15,9 @@ namespace Evlog.Infrastructure.DataModels
         public string Description { get; set; }
         public string Slug { get; set; }
         public string Body { get; set; }
-        public string Excerpt => Description ??  string.Join(" ", Body.Split().Take(25));
         public DateTime StartDateTime { get; set; }
         public DateTime? EndDate { get; set; }
         public DateTime? EndTime { get; set; }
-        public bool IsSingleDayEvent => StartDateTime.Date == (EndDate?.Date ?? StartDateTime.Date);
         public IEnumerable<AnnouncementDM> Announcements { get; set; }
     }
 }
