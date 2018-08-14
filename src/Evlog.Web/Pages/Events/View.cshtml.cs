c3929c9b0ce277f6f6337b7de20c391bc5a4c697
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-08-14 23:11:44 +0530

    Add a Register form to the ViewEvent page

--- src/Evlog.Web/Pages/Events/View.cshtml.cs
+++ src/Evlog.Web/Pages/Events/View.cshtml.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using Evlog.Domain.EventAggregate;
 using Evlog.Domain.Queries;
@@ -9,7 +10,11 @@ namespace Evlog.Web.Pages.Events
     public class ViewEventModel : PageModel
     {
         private readonly IEventQuery _eventQuery;
+
         public EventPost Post { get; set; }
+
+        [BindProperty]
+        public RegisterRequestVM RegisterVM { get; set; }
 
         public ViewEventModel(IEventQuery eventQuery)
         {
@@ -21,5 +26,21 @@ namespace Evlog.Web.Pages.Events
             Post = await _eventQuery.QueryAsync(slug);
             return Page();
         }
+
+        public async Task<IActionResult> OnPostAsync(string slug)
+        {
+            // TODO: Register the user for the event here
+
+            Post = await _eventQuery.QueryAsync(slug);
+            return Page();
+        }
+
+
+        public class RegisterRequestVM
+        {
+            [EmailAddress]
+            public string Email { get; set; }
+        }
+
     }
 }
