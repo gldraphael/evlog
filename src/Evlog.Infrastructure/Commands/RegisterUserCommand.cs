05459964fdb1b6cc4cd3ead8dfdd092c7c565dd8
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-08-22 21:27:23 +0530

    Raise & handle the registration initated event

--- src/Evlog.Infrastructure/Commands/RegisterUserCommand.cs
+++ src/Evlog.Infrastructure/Commands/RegisterUserCommand.cs
@@ -1,6 +1,8 @@
 using System.Threading.Tasks;
 using Evlog.Domain.EventAggregate.Commands;
 using Evlog.Domain.EventAggregate.Queries;
+using Evlog.Domain.Events;
+using Evlog.Domain.Events.Handlers;
 using Evlog.Infrastructure.DataModels;
 using MongoDB.Driver;
 
@@ -9,14 +11,19 @@ namespace Evlog.Infrastructure.Commands
     public class RegisterUserCommand : IRegisterUserCommand
     {
         private readonly IMongoCollection<EventPostDM> _events;
-        public RegisterUserCommand(IMongoCollection<EventPostDM> events)
+        private readonly IRegistrationInitiatedHandler _registrationInitiatedEventHandler;
+
+        public RegisterUserCommand(IMongoCollection<EventPostDM> events,
+            IRegistrationInitiatedHandler registrationInitiatedEventHandler)
         {
             _events = events;
+            _registrationInitiatedEventHandler = registrationInitiatedEventHandler;
         }
 
         public async Task Execute(string eventSlug, string userEmail)
         {
-            // TODO: raise RegistrationAttempted event
+            await _registrationInitiatedEventHandler.HandleAsync(
+                        new RegistrationInitiatedEvent(eventSlug, userEmail));
 
             var update = Builders<EventPostDM>.Update.AddToSet(x => x.Registrations, new RegistrationDM { Email = userEmail} );
             await _events.UpdateOneAsync(e => e.Slug == eventSlug, update);
