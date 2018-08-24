d80622090a898db866c039756c76a2f8f08973ef
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-08-24 11:56:30 +0530

    Setup integration tests (#28)
    * Add a simple integration test for the Index page
    * Use the correct SDK version
    * Spin up a mongo instance during build for the tests

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,8 +3,15 @@
 # This script builds docker images for the application.
 # NOTE: The script must be run from the project root directory.
 
+docker run --name mongo-testdb \
+    -e MONGO_INITDB_ROOT_USERNAME=root \
+    -e MONGO_INITDB_ROOT_PASSWORD=amDbDZ3v \
+    -p 27017:27017 \
+    -d --rm mongo
+
 docker build \
     -t gldraphael/evlog \
+    --network host \
     -f ./src/Evlog.Web/Dockerfile .
 
 docker build \
@@ -15,3 +22,5 @@ docker build \
     -t gldraphael/evlog-self-contained \
     -f ./src/Evlog.Web/Dockerfile \
     --target self-contained .
+
+docker stop mongo-testdb
---
+++ tests/Evlog.IntegrationTests/Pages/IndexPage_Should.cs
@@ -0,0 +1,27 @@
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Evlog.IntegrationTests.Pages
+{
+    public class IndexPage_Should : IClassFixture<TestFixture>
+	{
+		private readonly HttpClient _client;
+
+		public IndexPage_Should(TestFixture fixture)
+		{
+			_client = fixture.Client;
+		}
+
+		[Fact]
+		public async Task Request_ReturnsOK()
+		{
+			// Act
+			var response = await _client.GetAsync("/");
+
+			// Assert
+			Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+		}
+	}
+}
