21afd13f30b7b9133efa2d08373a97f46fd829f7
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-08-24 16:16:59 +0530

    Ensure a user is not registered more than once for an event

--- tests/Evlog.UnitTests/Infrastructure/Commands/RegisterUser_Should.cs
+++ tests/Evlog.UnitTests/Infrastructure/Commands/RegisterUser_Should.cs
@@ -23,8 +23,7 @@ namespace Evlog.UnitTests.Infrastructure.Commands
             db.Events.InsertOne(new EventPostDM {
                 Slug = slug
             });
-            var mock = new Mock<IRegistrationInitiatedHandler>();
-            var command = new RegisterUserCommand(db.Events, mock.Object);
+            var command = new RegisterUserCommand(db.Events, handler);
 
             // Act
             await command.Execute(eventSlug: slug, userEmail: email);
@@ -38,19 +37,38 @@ namespace Evlog.UnitTests.Infrastructure.Commands
         }
 
         [Fact]
-        public void Do_nothing_if_already_registered()
+        public async Task Do_nothing_if_already_registered_for_same_unit()
         {
+            // Arrange
+            const string slug = "hey-there";
+            const string email = "jane@doe.com";
+            db.Events.InsertOne(new EventPostDM {
+                Slug = slug
+            });
+            var command = new RegisterUserCommand(db.Events, handler);
 
+            // Act
+            await command.Execute(eventSlug: slug, userEmail: email);
+            await command.Execute(eventSlug: slug, userEmail: email);
+
+            // Assert
+            var @event = db.Events.Find(_ => _.Slug == slug).Single();
+            Assert.Single(@event.Registrations);
+
+            var registration = @event.Registrations.First();
+            Assert.Equal(email, registration.Email);
         }
 
 
 
         private readonly MongoDbContext db;
+        private readonly IRegistrationInitiatedHandler handler;
         public RegisterUser_Should()
         {
             db = new MongoDbContextBuilder()
                     .UseDefaultConfiguration()
                     .Build();
+            handler = new Mock<IRegistrationInitiatedHandler>().Object;
         }
 
         public void Dispose()
