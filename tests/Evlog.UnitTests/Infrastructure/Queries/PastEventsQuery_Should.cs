33d9556a30fc6cae08136cdddeda05f1c68ce36c
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-08-25 09:12:19 +0530

    Test to ensure PastEventsQuery returns empty collection

--- tests/Evlog.UnitTests/Infrastructure/Queries/PastEventsQuery_Should.cs
+++ tests/Evlog.UnitTests/Infrastructure/Queries/PastEventsQuery_Should.cs
@@ -33,5 +33,25 @@ namespace Evlog.UnitTests.Infrastructure.Queries
             // Assert
             Assert.Equal(2, result.Count);
         }
+
+        [Fact]
+        public async Task Return_empty_when_no_past_events_exist()
+        {
+            // Arrange
+            var date = DateTime.Today.AddDays(10).ToUniversalTime();
+            var posts = new List<EventPostDM>(new EventPostDM[] {
+                new EventPostDM { CreatedOn = date, StartDateTime = date, Slug = "well-im-gonna-keep-on-waking" },
+                new EventPostDM { CreatedOn = date, StartDateTime = date, Slug = "and-rising-up-before-the-sun" },
+                new EventPostDM { CreatedOn = date, StartDateTime = date, Slug = "and-lying-in-the-dark-wide-awake" },
+                new EventPostDM { CreatedOn = date, StartDateTime = date, Slug = "when-everybody-else-is-done" }
+            });
+            await Db.Events.InsertManyAsync(posts);
+
+            // Act
+            var result = await new PastEventsQuery(Db).QueryAsync();
+
+            // Assert
+            Assert.Empty(result);
+        }
     }
 }
