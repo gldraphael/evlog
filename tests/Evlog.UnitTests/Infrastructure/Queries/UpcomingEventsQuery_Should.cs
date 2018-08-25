09c26d06c28dcfb3b301b9ab24de2095c116c61e
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-08-25 09:18:59 +0530

    Tests for UpcomingEventsQuery

---
+++ tests/Evlog.UnitTests/Infrastructure/Queries/UpcomingEventsQuery_Should.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Evlog.Infrastructure.DataModels;
+using Evlog.Infrastructure.Queries;
+using Xunit;
+
+namespace Evlog.UnitTests.Infrastructure.Queries
+{
+    public class UpcomingEventsQuery_Should : MongoTestBed
+    {
+        [Fact]
+        public async Task Return_all_past_events()
+        {
+            // Arrange
+            var oldDate = DateTime.Today.AddDays(-10).ToUniversalTime();
+            var newDate = DateTime.Today.AddDays(10).ToUniversalTime();
+            var posts = new List<EventPostDM>(new EventPostDM[] {
+                new EventPostDM { CreatedOn = oldDate, StartDateTime = oldDate, Slug = "well-im-gonna-keep-on-waking" },
+                new EventPostDM { CreatedOn = oldDate, StartDateTime = oldDate, Slug = "and-rising-up-before-the-sun" },
+                new EventPostDM { CreatedOn = newDate, StartDateTime = newDate, Slug = "and-lying-in-the-dark-wide-awake" },
+                new EventPostDM { CreatedOn = newDate, StartDateTime = newDate, Slug = "when-everybody-else-is-done" }
+            });
+            await Db.Events.InsertManyAsync(posts);
+
+            // Act
+            var result = await new UpcomingEventsQuery(Db).QueryAsync();
+
+            // Assert
+            Assert.Equal(2, result.Count);
+        }
+
+        [Fact]
+        public async Task Return_empty_when_no_past_events_exist()
+        {
+            // Arrange
+            var date = DateTime.Today.AddDays(-10).ToUniversalTime();
+            var posts = new List<EventPostDM>(new EventPostDM[] {
+                new EventPostDM { CreatedOn = date, StartDateTime = date, Slug = "well-im-gonna-keep-on-waking" },
+                new EventPostDM { CreatedOn = date, StartDateTime = date, Slug = "and-rising-up-before-the-sun" },
+                new EventPostDM { CreatedOn = date, StartDateTime = date, Slug = "and-lying-in-the-dark-wide-awake" },
+                new EventPostDM { CreatedOn = date, StartDateTime = date, Slug = "when-everybody-else-is-done" }
+            });
+            await Db.Events.InsertManyAsync(posts);
+
+            // Act
+            var result = await new UpcomingEventsQuery(Db).QueryAsync();
+
+            // Assert
+            Assert.Empty(result);
+        }
+    }
+}
