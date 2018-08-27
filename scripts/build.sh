0729d7562997c0c38f59a163dca774106edd00f5
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-08-27 21:30:47 +0530

    Spawn mongo during build before running tests

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,18 +3,12 @@
 # This script builds docker images for the application.
 # NOTE: The script must be run from the project root directory.
 
-docker run --name mongo-testdb \
-    -p 27017:27017 \
-    -d --rm mongo
 
 docker build \
     -t gldraphael/evlog \
-    --network host \
     -f ./src/Evlog.Web/Dockerfile .
 
 docker build \
     -t gldraphael/evlog-self-contained \
     -f ./src/Evlog.Web/Dockerfile \
     --target self-contained .
-
-docker stop mongo-testdb
