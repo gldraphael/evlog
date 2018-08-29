6790e21f93be6350dddec3f6b796d1fdb2c1afe5
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-08-29 18:49:33 +0530

    Implement RegistrationCompletedHandler

--- src/Evlog.Domain/Events/Handlers/RegistrationCompletedEventHandler.cs
+++ src/Evlog.Domain/Events/Handlers/RegistrationCompletedEventHandler.cs
@@ -1,13 +1,20 @@
 using System.Threading.Tasks;
+using Evlog.Domain.UserAggregate.Commands;
 
 namespace Evlog.Domain.Events.Handlers
 {
     public class RegistrationCompletedHandler : IRegistrationCompletedHandler
     {
+        private readonly ICreateUserCommand createUserCommand;
+        public RegistrationCompletedHandler(ICreateUserCommand createUserCommand)
+        {
+            this.createUserCommand = createUserCommand;
+        }
+
         public Task HandleAsync(RegistrationCompletedEvent @event)
         {
-            // TODO: create a user if the user doesn't exist
-            return Task.FromResult(0);
+            // Create a new user when a registration is made
+            return createUserCommand.ExecuteAsync(@event.Email);
         }
     }
     public interface IRegistrationCompletedHandler : IEventHandler<RegistrationCompletedEvent>
