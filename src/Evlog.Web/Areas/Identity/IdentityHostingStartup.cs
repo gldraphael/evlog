9788f8e7f747a61e5f26a630d5c287ba20d3268f
Galdin Raphael <gldraphael@users.noreply.github.com>
2020-07-19 13:17:24 +0530

    Scaffold Identity UI files -- all of them for now

---
+++ src/Evlog.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Evlog.Web.Areas.Identity.Data;
+using Evlog.Web.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Evlog.Web.Areas.Identity.IdentityHostingStartup))]
+namespace Evlog.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<EvlogWebContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("EvlogWebContextConnection")));
+
+                services.AddDefaultIdentity<EvlogWebUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<EvlogWebContext>();
+            });
+        }
+    }
+}
