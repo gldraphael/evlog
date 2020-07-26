f656277675ee61e097444dd637a0bc56f8aff306
Galdin Raphael <gldraphael@users.noreply.github.com>
2020-07-26 17:28:49 +0530

    Use serilog (#105)

--- src/Evlog.Web/Program.cs
+++ src/Evlog.Web/Program.cs
@@ -2,6 +2,8 @@ using System.Threading.Tasks;
 using Evlog.Infrastructure.Extensions;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace Evlog.Web
 {
@@ -16,6 +18,14 @@ namespace Evlog.Web
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging((host, l) =>
+                {
+                    l.ClearProviders();
+                    var logger = new LoggerConfiguration()
+                                    .ReadFrom.Configuration(host.Configuration)
+                                    .CreateLogger();
+                    l.AddSerilog(logger);
+                })
                 .UseStartup<Startup>();
     }
 }
