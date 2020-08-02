c33c0d28312f3b67a57a0b026a782e4c77dde322
Galdin Raphael <gldraphael@users.noreply.github.com>
2020-08-02 14:24:04 +0530

    Simple create post page (no validation, no dates yet)

---
+++ src/Evlog.Web/Areas/Evlog/Pages/EventPosts/Create.cshtml.cs
@@ -0,0 +1,54 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Evlog.Core.Abstractions.Repositories;
+using Evlog.Core.Entities.EventAggregate;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Evlog.Web.Areas.Evlog.Pages.EventPosts
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ILogger<CreateModel> logger;
+        private readonly IEventPostRepository eventPosts;
+
+        public CreateModel(ILogger<CreateModel> logger, IEventPostRepository eventPosts)
+        {
+            this.logger = logger;
+            this.eventPosts = eventPosts;
+        }
+
+
+
+        [BindProperty] public CreateVM VM { get; set; } = new CreateVM();
+
+        public async Task<IActionResult> OnPost()
+        {
+            logger.LogDebug("Creating post with: {@CreateVM}", VM);
+            var post = new EventPost
+            {
+                Title = VM.Title ?? "Untitled",
+                Slug = new Slugify.SlugHelper().GenerateSlug(VM.Slug ?? VM.Title ?? $"untitled-{Guid.NewGuid()}"),
+                Description = VM.Description,
+                Body = VM.Markdown
+            };
+            await eventPosts.AddAsync(post);
+
+            return RedirectToPage("/Index", new { area = "Evlog" });
+        }
+
+        public class CreateVM
+        {
+            [Required]
+            public string? Title { get; set; }
+
+            public string? Slug { get; set; }
+            public string? Description { get; set; }
+
+            [Required]
+            public string? Markdown { get; set; }
+        }
+    }
+}
