b10529a2261a8c485d42231f2157e596dafbbc9d
Galdin Raphael <gldraphael@users.noreply.github.com>
2020-08-02 21:46:14 +0530

    Adjust the routes a bit

--- src/Evlog.Web/Areas/Evlog/Pages/Events/Create.cshtml.cs
+++ src/Evlog.Web/Areas/Evlog/Pages/Events/Create.cshtml.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace Evlog.Web.Areas.Evlog.Pages.EventPosts
+namespace Evlog.Web.Areas.Evlog.Pages.Events
 {
     public class CreateModel : PageModel
     {
