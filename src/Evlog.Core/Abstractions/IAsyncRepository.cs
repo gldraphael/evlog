d7609d6b75f3af6d2b9d73d1555775ace8727fb9
Galdin Raphael <gldraphael@users.noreply.github.com>
2020-08-23 22:34:58 +0530

    Don't use Ardalis.Specification
    Doesn't seem to make sense in this case

--- src/Evlog.Core/Abstractions/IAsyncRepository.cs
+++ src/Evlog.Core/Abstractions/IAsyncRepository.cs
@@ -1,4 +1,3 @@
-using Ardalis.Specification;
 using Evlog.Core.SharedKernel;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -9,12 +8,9 @@ namespace Evlog.Core.Abstractions
     {
         Task<T> GetByIdAsync(int id);
         Task<IReadOnlyList<T>> ListAllAsync();
-        Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
         Task<T> AddAsync(T entity);
         Task UpdateAsync(T entity);
         Task DeleteAsync(T entity);
-        Task<int> CountAsync(ISpecification<T> spec);
-        Task<T> FirstAsync(ISpecification<T> spec);
-        Task<T> FirstOrDefaultAsync(ISpecification<T> spec);
+        Task<int> CountAsync();
     }
 }
