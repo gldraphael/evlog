281c1d1d895857791c67bc5c0bdb212f3ca16064
Galdin Raphael <gldraphael@users.noreply.github.com>
2020-08-23 23:49:54 +0530

    login command implementation

--- src/Evlog.Core/Abstractions/ICommand.cs
+++ src/Evlog.Core/Abstractions/ICommand.cs
@@ -2,14 +2,15 @@ using MediatR;
 
 namespace Evlog.Core.Abstractions
 {
-    public interface ICommand : IRequest
-    {
+    public interface ICommand : IRequest { }
 
-    }
+    public interface ICommand<TResult> : IRequest<TResult> { }
 
-    public interface ICommandHandler<T> : IRequestHandler<T>
-        where T : ICommand
-    {
+    public interface ICommandHandler<TCommand> : IRequestHandler<TCommand>
+        where TCommand : ICommand
+    { }
 
-    }
+    public interface ICommandHandler<TCommand, TResult> : IRequestHandler<TCommand, TResult>
+        where TCommand : ICommand<TResult>
+    { }
 }
