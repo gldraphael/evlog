86d684fe5ba119bfc20ed6be2fb3ee56232427ba
Galdin Raphael <gldraphael@users.noreply.github.com>
2020-08-30 01:13:23 +0530

    Update the seed strategy to seed some future events

--- src/Evlog.Infrastructure/Data/SeedStrategies/DevSeedStrategy.cs
+++ src/Evlog.Infrastructure/Data/SeedStrategies/DevSeedStrategy.cs
@@ -53,6 +53,8 @@ namespace Evlog.Infrastructure.Data.SeedStrategies
                     }, DEFAULT_PASS);
             }
 
+            var today = DateTime.Today.AddHours(12);
+
             var seedEvents = new[]
             {
                 new EventPost
@@ -60,8 +62,8 @@ namespace Evlog.Infrastructure.Data.SeedStrategies
                     Title = "Building an Innovation Culture to enable the success of your Digital Transformation",
                     Description = "Lee Hickin and Veli-Matti will discuss how Australian businesses are building an innovation culture to enable the success of their digital transformation journeys.",
                     BodyMarkdown = "Businesses across all industries are talking about digital transformation. However, we need to be mindful that this transformation isn't just about the technology – it’s also about the people and the culture of an organisation that will enable this transformation to deliver value for your customers.   \n\nLee Hickin and Veli-Matti will discuss how Australian businesses are building an innovation culture to enable the success of their digital transformation journeys.   \n\nIn this free webinar, you'll learn about:   \n\n* The four pillars of transformation - vision & strategy, culture, unique potential and capabilities.\n* Creating a culture to increase your data maturity and deliver return on your transformation investments.\n* The rise of the citizen data scientist and the democratisation of AI",
-                    StartTimeUtc = new DateTimeOffset(year: 2020, month: 8, day: 4, hour: 11, minute:  0, second: 0, offset: TimeSpan.FromHours(10)).UtcDateTime,
-                    EndTimeUtc   = new DateTimeOffset(year: 2020, month: 8, day: 4, hour: 11, minute: 50, second: 0, offset: TimeSpan.FromHours(10)).UtcDateTime,
+                    StartTimeUtc = today.AddDays(7),
+                    EndTimeUtc   = today.AddDays(7).AddHours(1),
                     Slug = "building-an-innovation-culture-to-enable-the-success-of-your-digital-transformation"
                 },
 
@@ -70,8 +72,8 @@ namespace Evlog.Infrastructure.Data.SeedStrategies
                     Title = "Get Kubernetes Up and Running",
                     Description = "Join this free webinar to learn how to get started with Kubernetes open-source container orchestration software and get a sneak peek of the second edition of Kubernetes from co-founder Brendan Burns.",
                     BodyMarkdown = "Join this free webinar to learn how to get started with Kubernetes open-source container orchestration software and get a sneak peek of the second edition of Kubernetes from co-founder Brendan Burns.   \n\nYou’ll also learn about: \n* Answers to frequently asked questions such as “What should I put in a pod?”\n* Using different Kubernetes objects in tandem to achieve reliable software rollout. \n* Using a managed Kubernetes service to simplify day-to-day operations such as upgrading, patching, and scaling.",
-                    StartTimeUtc = new DateTimeOffset(year: 2020, month: 8, day: 13, hour: 11, minute: 0, second: 0, offset: TimeSpan.FromHours(10)).UtcDateTime,
-                    EndTimeUtc   = new DateTimeOffset(year: 2020, month: 8, day: 13, hour: 12, minute: 0, second: 0, offset: TimeSpan.FromHours(10)).UtcDateTime,
+                    StartTimeUtc = today.AddDays(14),
+                    EndTimeUtc   = today.AddDays(14).AddHours(1),
                     Slug = "get-kubernetes-up-and-running"
                 },
                 new EventPost
