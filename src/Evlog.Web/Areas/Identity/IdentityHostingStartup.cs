c266bacf35760ca83f5b4469d84501a4141ffb31
Galdin Raphael <gldraphael@users.noreply.github.com>
2020-09-06 18:01:17 +0530

    Configure identity / csrf cookies

--- src/Evlog.Web/Areas/Identity/IdentityHostingStartup.cs
+++ src/Evlog.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -3,6 +3,7 @@ using Evlog.Infrastructure.Data.DataModels;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 [assembly: HostingStartup(typeof(Evlog.Web.Areas.Identity.IdentityHostingStartup))]
 namespace Evlog.Web.Areas.Identity
@@ -25,7 +26,34 @@ namespace Evlog.Web.Areas.Identity
                     options.Password.RequiredLength = 12; // TODO: make this configurable
                 })
                     .AddEntityFrameworkStores<AppDbContext>();
+
+                services.ConfigureApplicationCookie(options =>
+                {
+                    options.AccessDeniedPath = "/identity/account/accessdenied";
+                    options.ExpireTimeSpan = TimeSpan.FromDays(14);
+                    //options.LoginPath = "/Identity/Account/Login"; // TODO: Passwordless login page
+                    options.SlidingExpiration = true;
+
+                    options.Cookie.Name = "__evlog_auth";
+                    options.Cookie.HttpOnly = true;
+                    options.Cookie.IsEssential = true;
+                    options.Cookie.SameSite = Microsoft.AspNetCore.Http.SameSiteMode.Strict;
+                });
+
+                services.AddAntiforgery(options =>
+                {
+                    options.FormFieldName = "__xsrf";
+                    options.HeaderName = "X-CSRF-TOKEN";
+                    options.SuppressXFrameOptionsHeader = false;
+
+                    options.Cookie.Name = "__evlog_xsrf";
+                    options.Cookie.IsEssential = true;
+                    options.Cookie.HttpOnly = true;
+                    options.Cookie.SameSite = Microsoft.AspNetCore.Http.SameSiteMode.Strict;
+                });
             });
+
+
         }
     }
 }
